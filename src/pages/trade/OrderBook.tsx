8d8742b5181108196f5028b93cf797e448c9da1c
DoniPop <minjaebae@gmail.com>
2023-03-19 18:23:14 +0900

    orderBook기초틀 제작

--- src/pages/trade/OrderBook.tsx
+++ src/pages/trade/OrderBook.tsx
@@ -2,36 +2,43 @@ import styles from "./Styles/orderBook.module.css";
 export default function orederBook() {
     return (
         <main>
-            <table>
+            <table className={styles.table}>
                 <colgroup>
-                    <col width={42}></col>
-                    <col width={120}></col>
-                    <col width={"*"}></col>
+                    <col width={160}></col>
+                    <col width={180}></col>
+                    <col width={160}></col>
                 </colgroup>
                 <tbody>
-                    <tr>
+                    <tr className={styles.sellTr}>
                         <td className={styles.bar}>
-                            <div style={{ width: "35.835%" }}></div>
-                            <p>0.001</p>
+                            <div style={{ width: "5%" }} className={`${styles.barChild} ${styles.sell}`}>
+                                <p>0.001</p>
+                            </div>
                         </td>
-                        <td className={styles.price}>20,000</td>
+                        <td className={styles.priceSell}>20,000</td>
+                        <td></td>
                     </tr>
 
-                    <tr>
+                    <tr className={styles.tr}>
                         <td className={styles.bar}>
-                            <div style={{ width: "35.835%" }}></div>
-                            <p>0.001</p>
+                            <div style={{ width: "35%" }} className={`${styles.barChild} ${styles.sell}`}>
+                                <p>123,456,789,012,345,789</p>
+                            </div>
                         </td>
-                        <td className={styles.price}></td>
+                        <td className={styles.price}>10,000</td>
+                        <td></td>
                     </tr>
 
-                    <tr>
-                        <td className={styles.bar}>
-                            <div style={{ width: "35.835%" }}></div>
-                            <p>0.001</p>
+                    <tr className={styles.buyTr}>
+                        <td></td>
+                        <td className={styles.priceBuy}>10,000</td>
+                        <td className={styles.barBuy}>
+                            <div style={{ width: "35%" }} className={`${styles.barChild} ${styles.buy}`}>
+                                <p>123,456,789,012,345,789</p>
+                            </div>
                         </td>
-                        <td className={styles.price}></td>
                     </tr>
+
                 </tbody>
             </table>
         </main>
